810994a7e9c8e12d1b8f03959ace38e471da7742
diogeneshas <diogenes.henrique.br@gmail.com>
2022-05-27 08:08:38 -0300

    Adds the products create, update and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
-      resources :tokens, only: %i[:create]
-      resources :products, only: %i[:show]
+      resources :tokens, only: [:create]
+      resources :products
     end
   end
 end
